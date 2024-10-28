e48a7dd1a3274a3faf3f37b887b2a4e3062d2fce
LauraS141 <90742248+LauraS141@users.noreply.github.com>
2024-10-28 18:46:24 -0400

    Added simple DataPipeline class

--- app.py
+++ app.py
@@ -42,8 +42,56 @@ You will receive input in one of two formats:
 Maintain a conversational tone and ensure your response is appropriate to the input received.
 '''
 
+class DataPipeline:
 
-class App:
+    def __init___(self):
+        self.chat_history = {}
+    
+    def get_inputs(self, session_id):
+
+        if self.chat_history[session_id]:
+            for msg in self.chat_history[session_id]:
+                if msg.role == "user":
+                    print("\nUser Input:" + msg.content)
+
+    def get_outputs(self, session_id):
+
+        if self.chat_history[session_id]:
+            for msg in self.chat_history[session_id]:
+                if msg.role == "system":
+                    print("\nSystem Output:" + msg.content)
+    
+    def get_full_chat_history(self, session_id, output_file):
+
+        file = open(output_file, "w") 
+
+        chat = self.chat_history[session_id]
+
+        file.write("CHAT HISTORY " + session_id + "\n")
+        for msg in chat:
+            if msg.role == "user":
+                file.write("USER: " + msg.content + "\n")
+            
+            else:
+                file.write("SYSTEM: " + msg.content + "\n")
+
+        file.close() 
+                    
+
+    def add_message(self, role, content, session_id):
+        msg = Message(role, content)
+        self.chat_history[session_id].append(msg)
+
+
+class Message:
+    _role: str
+    _content: str
+
+    def __init__(self, role, content):
+        self._role = role
+        self._content = content
+
+class App:    
     _model: ChatOpenAI
     _tools: list[BaseTool]
     _memory: MemorySaver
@@ -56,15 +104,20 @@ class App:
         self._memory = MemorySaver()
         self._agent1_executer = create_react_agent(self._model, self._tools, state_modifier=prompt1, checkpointer=self._memory)
         self._agent2_executer = create_react_agent(self._model, self._tools, state_modifier=prompt2, checkpointer=self._memory)
+        self._pipeline = DataPipeline()
 
     def submit_message(self, message: str, session_id: str) -> str:
         config = {"configurable": {"thread_id": session_id}}
+
+        self._pipeline.add_message("user", message, session_id)
 
         response1 = self._agent1_executer.invoke({"messages": [HumanMessage(content=message)]}, config)
 
         clarification_result = response1["messages"][-1].content
 
         response2 = self._agent2_executer.invoke({"messages": [HumanMessage(content=message + clarification_result)]}, config)
+        self._pipeline.add_message("system", response2, session_id)
+
 
         return response2['messages'][-1].content
 
@@ -78,4 +131,4 @@ if __name__ == "__main__":
     while True:
         message = input("ME: ")
         response = app.submit_message(message, curr_session_id)
-        print("SYSTEM: " + response)
+        app._pipeline.get_full_chat_history(curr_session_id, "output.txt")
